7de9b615ba49b94d0ce3b02b99c73c649e20d8de
Михаил <1628191+bitbybit@users.noreply.github.com>
2022-01-03 21:31:26 +0300

    paginator

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,15 +1,20 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render, get_object_or_404
-
+from django.core.paginator import Paginator
 from .models import Post, Group
 
 POSTS_LIMIT = 10
 
 
 def index(request: HttpRequest) -> HttpResponse:
+    posts = Post.objects.all()
+
+    paginator = Paginator(posts, POSTS_LIMIT)
+    page_number = request.GET.get("page")
+
     context = {
         "title": "Последние обновления на сайте",
-        "posts": Post.objects.all()[:POSTS_LIMIT],
+        "page_obj": paginator.get_page(page_number),
     }
 
     return HttpResponse(render(request, "posts/index.html", context))
@@ -17,10 +22,14 @@ def index(request: HttpRequest) -> HttpResponse:
 
 def group_posts(request: HttpRequest, slug: str) -> HttpResponse:
     group = get_object_or_404(Group, slug=slug)
+    posts = group.posts.all()
+
+    paginator = Paginator(posts, POSTS_LIMIT)
+    page_number = request.GET.get("page")
 
     context = {
         "title": f"Записи сообщества {group}",
-        "posts": group.posts.all(),
+        "page_obj": paginator.get_page(page_number),
     }
 
     return HttpResponse(render(request, "posts/group_list.html", context))
