612c5b51ba1c8e2f243390bae0f862ca21defc0c
Михаил <1628191+bitbybit@users.noreply.github.com>
2022-01-04 22:35:09 +0300

    pr fixes

--- yatube/core/context_processors/year.py
+++ yatube/core/context_processors/year.py
@@ -1,8 +1,7 @@
-from datetime import datetime
-
 from django.http import HttpRequest
+from django.utils import timezone
 
 
 def year(request: HttpRequest):
     """Добавляет переменную с текущим годом."""
-    return {"year": int(datetime.now().strftime("%Y"))}
+    return {"year": int(timezone.now().strftime("%Y"))}
